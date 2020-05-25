d6f89ea82acb2c85fdcf4db525944aa0686f5f4a
amitabhis2903 <amitabhis2903@gmail.com>
2020-05-25 17:58:56 +0530

    project initialzed

---
+++ MultipleCellTableViewMVVM/MultipleCellTableViewMVVM/Controller/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  MultipleCellTableViewMVVM
+//
+//  Created by Amitabh Pandey on 25/05/20.
+//  Copyright © 2020 AmitabhPandey. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        // Do any additional setup after loading the view.
+    }
+}
